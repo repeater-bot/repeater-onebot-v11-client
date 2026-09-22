0180c8d2748a5a65d59d95d2a43b48f3db6600e5
Qeggs <get_boot@outlook.com>
2026-09-22 18:21:54 +0800

    refactor(external_trigger_server): 将主机地址从localhost改为127.0.0.1
    将外部触发服务器配置中的主机地址从"localhost"更改为"127.0.0.1"

--- Repeater/repeater/plugins/repeater_main/client_configs/main_configs/external_trigger_server.py
+++ Repeater/repeater/plugins/repeater_main/client_configs/main_configs/external_trigger_server.py
@@ -5,5 +5,5 @@ class ExternalTriggerServer(BaseModel):
     External trigger server configuration.
     """
     enabled: bool = False
-    host: str = "localhost"
+    host: str = "127.0.0.1"
     port: int = 5000
